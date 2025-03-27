1d57ca46a4e8385f8c8969bb567a59c495c0a95e
carret938 <carr.erik@epa.gov>
2025-03-26 20:01:55 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,271 @@
+# Chemical Functional Use Taxonomy (ChemFUT)
+
+The Analytical Methods and Open Spectra (AMOS) Database's Chemical Functional Use Taxonomy (ChemFUT) contains mappings between chemicals (via name and DTXSID) and their functional uses. This repository contains a snapshot of the SQLite database and a Python class to help query the database.
+
+## ChemFUT Database
+
+A snapshot of the data is contained in `data/ChemFUT.db`. Here is an ER diagram representation of the database.
+
+![ChemFUT ER Diagram](./resources/ER_diagram.png)
+
+Descriptions of each table and their variables follow:
+
+| Table | Description |
+|-------|-------------|
+| Sources | Contains unique identifiers for each source that data was pulled from. |
+| Chemicals | Maps the unique chemical identifier (DTXSID) to its chemical name. |
+| Classifications | Maps a unique classification ID to a human readable class name (e.g., Pharmaceuticals) and a description of that class. |
+| ChemicalClassifications | Maps chemicals (by DTXSID) to their classifications, and which source that classification came from. |
+| SourceMappings | Contains the raw category pulled from a given source mapped to a harmonized ChemFUT class. |
+| ClassificationHierarchy | Contains parent/child mappings for each classification. |
+
+### Sources Table
+
+| Variable | Role | Description |
+|----------|------|-------------|
+| source_id | PK  | A unique identifier for each source. E.g., "wikipedia", "drugbank". |
+
+### Chemicals Table
+
+| Variable | Role | Description |
+|----------|------|-------------|
+| dtxsid   | PK   | The unique DTXSID for each chemical. |
+| name     |      | The EPA's preferred name for each chemical. |
+
+### Classifications Table
+
+| Variable | Role | Description |
+|----------|------|-------------|
+| id       | PK   | A unique identifier for each classification of the form func_0001. |
+| classification || A human readable name for each classification (e.g., Pharmaceuticals). |
+| description |   | A description for each classification. |
+
+### ChemicalClassifications Table
+
+| Variable | Role | Description |
+|----------|------|-------------|
+| dtxsid   | CPK, FK $\rightarrow$ `Chemicals.dtxsid` | The unique DTXSID chemical identifier. |
+| classification_id | CPK, FK $\rightarrow$ `Classifications.id` | A unique identifier for each classification. |
+| source_id | CPK, FK $\rightarrow$ `Sources.source_id` | The unique source_id for each source. |
+
+### SourceMappings Table
+
+| Variable | Role | Description |
+|----------|------|-------------|
+| id       | CPK, FK $\rightarrow$ `Classifications.id` | A unique identifier for each classification. |
+| source_category | CPK | The raw category pulled from the source that was mapped to the given `id`. |
+| source_id | CPK, FK $\rightarrow$ `Sources.source_id | The unique source_id for each source. |
+
+### ClassificationHierarchy
+
+| Variable | Role | Description |
+|----------|------|-------------|
+| child_id | CPK, FK $\rightarrow$ `Classifications.id` | The unique classification identifier for the child node. |
+| parent_id | CPK, FK $\rightarrow$ `Classifications.id` | The unique classification identifier for the parent node. |
+
+## Usage
+
+For the chemFUT.ChemFUTHelper class to work by default, you must use the following directory structure:
+
+![Expected directory structure](./resources/directory_tree.png)
+
+For more information about any given method and its parameters, check the respective docstring in the source code.
+
+### Connecting to ChemFUT.db
+
+If using the recommended directory structure, the path to ChemFUT.db is correctly set within the `sqlite_handler.SqliteHandler.chem_func_uses_path` attribute such that instantiating an instance of `chemFUT.ChemFUTHelper` will automatically connect to the database and set the resulting `sqlite3.Connection` and `sqlite3.Cursor` objects to the `chemFUT.ChemFUTHelper.conn` and `chemFUT.ChemFUTHelper.cursor` attributes, respectively.
+
+```python
+from chemFUT import ChemFUTHelper
+
+FuncDB = ChemFUTHelper()
+```
+
+If using a different directory structure, you can specify the path of your `.db` file when instantiating `chemFUT.ChemFUTHelper` as a string or a `pathlib.Path` object.
+
+```python
+FuncDB = ChemFUTHelper("./path/to/ChemFUT.db")
+```
+
+### Print Database/Table Descriptions
+
+#### ChemFUTHelper.print_db_description()
+
+This method will print a description of each table with its columns to the console.
+
+```python
+FuncDB = ChemFUTHelper()
+FuncDB.print_db_description()
+```
+
+Example output:
+
+```dummy
+Table: Chemicals
+  Column: dtxsid, Type: TEXT, Not Null: 0, Default: None, Primary Key: 1
+  Column: name, Type: TEXT, Not Null: 1, Default: None, Primary Key: 0
+ ----------------------------------------
+Table: Classifications
+  Column: id, Type: TEXT, Not Null: 0, Default: None, Primary Key: 1
+  Column: classification, Type: TEXT, Not Null: 1, Default: None, Primary Key: 0
+  Column: description, Type: TEXT, Not Null: 0, Default: None, Primary Key: 0
+----------------------------------------
+Table: ChemicalClassifications
+  Column: dtxsid, Type: TEXT, Not Null: 1, Default: None, Primary Key: 1
+  Column: classification_id, Type: TEXT, Not Null: 1, Default: None, Primary Key: 2
+  Column: source_id, Type: TEXT, Not Null: 1, Default: None, Primary Key: 3
+----------------------------------------
+Table: ClassificationHierarchy
+  Column: child_id, Type: TEXT, Not Null: 1, Default: None, Primary Key: 1
+  Column: parent_id, Type: TEXT, Not Null: 0, Default: None, Primary Key: 2
+----------------------------------------
+Table: SourceMappings
+  Column: id, Type: TEXT, Not Null: 1, Default: None, Primary Key: 1
+  Column: source_category, Type: TEXT, Not Null: 1, Default: None, Primary Key: 2
+  Column: source_id, Type: TEXT, Not Null: 1, Default: None, Primary Key: 3
+----------------------------------------
+Table: Sources
+  Column: source_id, Type: TEXT, Not Null: 1, Default: None, Primary Key: 1
+----------------------------------------
+```
+
+#### ChemFUTHelper.print_table()
+
+This method will print the contents of a table from the database. The user specifies the number of rows they want printed. To print the first 10 rows of the `SourceMappings` table:
+
+```python
+FuncDB = ChemFUTHelper()
+FuncDB.print_table(table_name="SourceMappings", limit=10)
+```
+
+To print every row of the `Sources` table:
+
+```python
+FuncDB = ChemFUTHelper()
+FuncDB.print_table(table_name="Sources", limit=None)
+```
+
+### Making Queries
+
+#### ChemFUTHelper.query_hierarchy_paths()
+
+This method will return every possible hierarchical path starting from each root node. The returned data structure is by default a tuple of two lists of strings. The first list contains the paths with the classes encoded with their classification id (e.g., func_0004), the second list contains the paths with the class names. Each element represents one possible path (e.g., ['Drugs -> Pharmaceuticals -> Respiratory Drugs -> Anti-allergic Agents', 'Drugs -> Pharmaceuticals -> Respiratory Drugs -> Anti-allergic Agents -> Antihistamines']). Notice that the delimiter ' -> ' points from parent to child.
+
+This method might be useful for further processing, things like determining the longest path from a root node to a leaf node.
+
+```python
+FuncDB = ChemFUTHelper()
+id_paths, class_name_paths = FuncDB.query_hierarchy_paths()
+```
+
+#### ChemFUTHelper.get_chem_name()
+
+Takes a DTXSID as a required parameter and returns the chemical name.
+
+```python
+FuncDB = ChemFUTHelper()
+dtxsid = "DTXSID9020112"
+chem_name = FuncDB.get_chem_name(dtxsid)
+print(chem_name)
+```
+
+prints
+> \>\> Atrazine
+
+#### ChemFUTHelper.get_class_id_from_name()
+
+Takes a class name and returns its class id.
+
+```python
+FuncDB = ChemFUTHelper()
+class_name = "Pharmaceuticals"
+class_id = FuncDB.get_class_id_from_name(class_name)
+print(class_id)
+```
+
+prints
+> \>\> func_0231
+
+#### ChemFUTHelper.get_class_name_from_id()
+
+Takes a class id and returns its name.
+
+```python
+FuncDB = ChemFUTHelper()
+class_id = "func_0231"
+class_name = FuncDB.get_class_id_from_name(class_id)
+print(class_name)
+```
+
+prints
+> \>\> Pharmaceuticals
+
+#### ChemFUTHelper.get_chem_classes()
+
+Returns the classes that a chemical (from DTXSID) is a member of. This method is fairly robust in that it allows many alterations of your output through the optional parameters.
+
+It should be noted that the hierarchy of classes is not retained in the returned value - it is either a list of classes or a semicolon delimited string.
+
+To return all classes that Atrazine is a member of, from all sources:
+
+```python
+FuncDB = ChemFUTHelper()
+dtxsid = "DTXSID9020112"
+atrazine_classes = FuncDB.get_chem_classes(dtxsid)
+print(atrazine_classes)
+```
+
+prints
+> \>\> Additives; Biocides; Biologicals; Fertilizers; Herbicides; Hormones; Industrial Chemicals; Pesticides; Soil Additives; Xenohormones
+
+To only get the classes for Atrazine that came from APPRIL in a list:
+
+```python
+FuncDB = ChemFUTHelper()
+dtxsid = "DTXSID9020112"
+atrazine_classes = FuncDB.get_chem_classes(dtxsid, sources=["appril"], as_str=False)
+print(atrazine_classes)
+```
+
+prints
+> \>\> ['Additives', 'Biocides', 'Fertilizers', 'Herbicides', 'Industrial Chemicals', 'Pesticides', 'Soil Additives']
+
+To return the classification IDs instead of the class names:
+
+```python
+FuncDB = ChemFUTHelper()
+dtxsid = "DTXSID9020112"
+atrazine_classes = FuncDB.get_chem_classes(dtxsid, names=False, sources=["appril"], as_str=False)
+print(atrazine_classes)
+```
+
+prints
+> \>\> ['func_0005', 'func_0087', 'func_0153', 'func_0181', 'func_0189', 'func_0227', 'func_0269']
+
+#### ChemFUTHelper.get_class_parents
+
+Returns the direct parents of a given class (accepts either class name or class id).
+
+```python
+FuncDB = ChemFUTHelper()
+class_name = "Antinematodal Agents"
+parents = FuncDB.get_class_parents(class_name)
+print(parents)
+```
+
+prints
+> \>\> ['Anthelmintics', 'Nematicides']
+
+#### ChemFUTHelper.get_class_children()
+
+Returns the direct children of a given class (accepts either class name or class id).
+
+```python
+FuncDB = ChemFUTHelper()
+class_name = "Biocides"
+children = FuncDB.get_class_parents(class_name)
+print(children)
+```
+
+prints
+> \>\> ['Acaricides', 'Algicides', 'Antifouling Agents', 'Antimicrobial Agents', 'Antimycotics', 'Antiparasitics', 'Avicides', 'Chemosterilants', 'Fumigants', 'Fungicides', 'Fungistats', 'Herbicides', 'Insecticides', 'Molluscicides', 'Nematicides', 'Spermicides', 'Sporicide', 'Sterilizing Agents']
---
+++ src/chemFUT.py
@@ -0,0 +1,352 @@
+"""Handles tasks associated with the Chemical Functional Use Taxonomy sqlite database.
+
+E. Tyler Carr
+November 21, 2024
+"""
+from pathlib import Path
+from typing import Literal
+
+from sqlite_handler import SqliteHandler
+
+class ChemFUTHelper(SqliteHandler):
+    """Class representing a connection to the Functional Use Classification DB."""
+    
+    def __init__(self, path: str | Path | None = None):
+        """Constructor for ChemFUTHelper objects.
+        
+        Inherits from amos.src.sqlite_handler.SqliteHandler class.
+        
+        Automatically instantiates a connection and cursor to the db.
+        
+        Parameters
+        ----------
+        path : str | pathlib.Path | None, default None
+            The path to the ChemFUTHelper sqlite .db file. If None, uses the parent
+            class sqlite_handler.SqliteHandler attribute.
+        """
+        super().__init__()
+        
+        if path is None:
+            self.set_conn(self.chem_func_uses_path)
+        else:
+            if not self._has_db_suffix(path):
+                raise ValueError(f"path must point to a .db file, not {path}")
+            path = Path(str(path)).resolve()
+            self.set_conn(path)
+    
+    def query_hierarchy_paths(self, as_str: bool = False) -> tuple[str, str] | tuple[list[str]]:
+        """Returns a string of ' -> ' delimited paths for all hierarchy paths starting
+        from root nodes on. The first str contains the classification_ids, the
+        second str contains the classification names.
+        
+        Parameters
+        ----------
+        as_str : bool, default True
+            If True, returns results in a ' -> ' delimited string 
+            (e.g., "Industrial Chemicals -> Additives -> Fillers")
+            Else, returns an ordered list of the paths instead
+            (e.g., ["Industrial Chemicals", "Additives", "Fillers"])
+        """
+        
+        ## Make query
+        self.cursor.execute("""
+            -- Recursive call to get parent/child relationships
+            WITH RECURSIVE HierarchyPaths AS (
+              -- Start with the root nodes
+              SELECT parent_id, child_id, child_id AS Path
+              FROM ClassificationHierarchy
+              WHERE parent_id is NULL
+                
+              UNION ALL
+                
+              -- For each child, append parent to the path
+              SELECT h.parent_id, h.child_id, Path ||' -> '|| h.child_id
+              FROM ClassificationHierarchy h
+              JOIN HierarchyPaths hp ON h.parent_id = hp.child_id
+            )
+            -- Select the full path for leaf nodes
+            SELECT Path
+            FROM HierarchyPaths
+            """)
+        
+        ## Create the ordered lists
+        paths = self.cursor.fetchall()
+        class_name_paths = []
+        class_code_paths = []
+        for path in paths:
+            path = path[0]
+            new_path = []
+            for func_id in path.split(" -> "):
+                self.cursor.execute(f"SELECT classification FROM Classifications WHERE id = ?",
+                                    (func_id,))
+                new_path.append(self.cursor.fetchone()[0])
+            new_path = " -> ".join(new_path)
+            class_name_paths.append(new_path)
+            class_code_paths.append(path)
+        
+        ## optionally convert to str
+        if as_str:
+            class_name_paths = " | ".join(class_name_paths)
+            class_code_paths = " | ".join(class_code_paths)
+        
+        return class_code_paths, class_name_paths
+    
+    def get_chem_name(self, dtxsid: str) -> str:
+        """Returns the preferred name of dtxsid"""
+        self.cursor.execute("""
+            SELECT name FROM Chemicals
+            WHERE dtxsid is ?;             
+            """, (dtxsid,))
+        
+        return self.cursor.fetchone()[0]
+        
+    def get_class_id_from_name(self, name: str) -> str:
+        """Returns the classification id that matches to name."""
+        self.cursor.execute("""
+               SELECT id FROM Classifications
+               WHERE classification is ?;             
+            """, (name,))
+        
+        return self.cursor.fetchone()[0]
+    
+    def get_class_name_from_id(self, func_id: str) -> str:
+        """Returns the classification label from func_id."""
+        
+        self.cursor.execute("""
+            SELECT classification FROM Classifications
+            WHERE id is ?;             
+            """, (func_id,))
+        
+        classification = self.cursor.fetchone()[0]
+        
+        return classification
+        
+    def get_chem_classes(self, dtxsid: str, names: bool = True, only_direct_parent: bool = False, 
+                         sources: None | list[Literal["wikipedia", "appril", "drugbank", "chemexpo"]] = None, 
+                         as_str: bool = True) -> str | list[str]:
+        """Returns the unique classes for a given dtxsid.
+        
+        This method is horrendous, but it works.
+        
+        Parameters
+        ----------
+        dtxsid : str
+            The DTXSID for the chemical you want the classes of.
+        names : bool, default True
+            If True, outputs the names of classes. Else outputs the func_ids.
+        only_direct_parent : bool, default False
+            If True, only returns the direct parent classes of the chemical
+            (ignores hierarchically implied classes).
+            Else, parents ancestry tree will be followed to get all class memberships.
+        sources : None | list[literal["wikipedia", "appril", "drugbank", "chemexpo"]], default None
+            A list of sources to pull classifications from. If None, all sources are used.
+        as_str : bool, default True
+            If True, outputs a semicolon delimited str of classes (e.g., "Drugs; Pharmaceuticals")
+            Else, outputs a list of classes (e.g., ["Drugs", "Pharmaceuticals"])
+        
+        Returns
+        -------
+        str | list[str]
+            The functional use classes of dtxsid (either the ids or the names).
+        """
+        if type(sources) == list:
+            for source in sources:
+                if source.lower() not in ["wikipedia", "appril", "drugbank", "chemexpo"]:
+                    raise ValueError("Sources must be None or a list containing one or more of the following literals: 'wikipedia', 'appril', 'drugbank', 'chemexpo'.")
+        ## get the direct parent classification_ids
+        if sources == None:
+            self.cursor.execute("""
+                SELECT classification_id FROM ChemicalClassifications
+                WHERE dtxsid = ?                
+                """, (dtxsid,))
+        else:
+            placeholders = ', '.join('?' for _ in sources)
+            self.cursor.execute(f"""
+                SELECT classification_id FROM ChemicalClassifications
+                WHERE dtxsid = ?         
+                AND
+                source_id IN ({placeholders})       
+                """, (dtxsid, *sources))
+        direct_parent_ids = list(set([parent_id[0] for parent_id in self.cursor.fetchall()]))
+        
+        ## if just looking for direct parents, we can return now
+        if ((not names) and only_direct_parent):
+            if as_str:
+                direct_parent_ids = "; ".join(direct_parent_ids)
+                
+            return direct_parent_ids
+        elif only_direct_parent:
+            direct_parent_names = set()
+            for direct_parent_id in direct_parent_ids:
+                direct_parent_name = self.get_class_name_from_id(direct_parent_id)
+                direct_parent_names.add(direct_parent_name)
+            
+            direct_parent_names = list(direct_parent_names)
+            direct_parent_names.sort()
+            
+            if as_str:
+                direct_parent_names = "; ".join(direct_parent_names)
+            
+            return direct_parent_names
+        
+        ## get the parent's parents recursively
+        ancestry_ids = set()
+        for parent_id in direct_parent_ids:
+            self.cursor.execute("""
+                WITH RECURSIVE HierarchyAncestry AS (
+                    -- start ancestry with direct parent node
+                    SELECT parent_id, child_id, child_id AS Ancestry
+                    FROM ClassificationHierarchy
+                    WHERE child_id IS ?
+                    
+                    UNION ALL
+                    
+                    -- for each ancestor, append the ancestry
+                    SELECT h.parent_id, h.child_id, Ancestry ||' -> '|| h.parent_id
+                    FROM ClassificationHierarchy h
+                    JOIN HierarchyAncestry ha ON ((h.child_id = ha.parent_id) AND (h.parent_id IS NOT NULL))
+                )
+                SELECT Ancestry
+                FROM HierarchyAncestry;
+                """, (parent_id,))
+            
+            ancestry_strs = [path[0] for path in self.cursor.fetchall()]
+            for path in ancestry_strs:
+                for p in path.split(" -> "):
+                    ancestry_ids.add(p)
+                    
+        ancestry_ids = list(ancestry_ids)
+        ancestry_ids.sort()
+        
+        if not names:
+            if as_str:
+                ancestry_ids = "; ".join(ancestry_ids)
+            return ancestry_ids
+        
+        ## get the names
+        ancestry_names = set()
+        for ancestry_id in ancestry_ids:
+            ancestry_name = self.get_class_name_from_id(ancestry_id)
+            ancestry_names.add(ancestry_name)
+            
+        ancestry_names = list(ancestry_names)
+        ancestry_names.sort()
+            
+        if not names:
+            if as_str:
+                ancestry_ids = "; ".join(ancestry_ids)
+                
+            return ancestry_ids
+        else:
+            if as_str:
+                ancestry_names = "; ".join(ancestry_names)
+                
+            return ancestry_names
+    
+    def get_class_parents(self, node: str, names: bool = True) -> list[str]:
+        """Returns a list of parents to the node class.
+        
+        Parameters
+        ----------
+        node : str
+            The name of the class of interest. Can be the class id,
+            or class name -- class_id always starts with func_, so 
+            it is easy to parse.
+        names : bool, default True
+            If True, outputs the names of classes. Else outputs the func_ids.
+            
+        Returns
+        -------
+        list[str]
+            The list of parents of the functional use class node.
+        """
+        if node.startswith("func_"):
+            self.cursor.execute("""
+                SELECT parent_id FROM ClassificationHierarchy
+                WHERE ((child_id is ?) AND (parent_id IS NOT NULL));               
+                """, (node,))    
+        else:
+            self.cursor.execute("""
+                SELECT parent_id FROM ClassificationHierarchy
+                WHERE child_id = (
+                    SELECT id FROM Classifications
+                    WHERE classification = ?
+                );
+                """, (node,))
+        
+        parents = set()
+        for row in self.cursor.fetchall():
+            parents.add(row[0])
+            
+        parents = list(parents)
+        
+        if names:
+            new_parents = set()
+            for parent_id in parents:
+                self.cursor.execute("""
+                    SELECT classification FROM Classifications
+                    WHERE id = ?;                
+                    """, (parent_id,))
+                new_parents.add(self.cursor.fetchone()[0])
+            
+            parents = list(new_parents)
+            
+        parents.sort()
+        
+        return parents
+    
+    def get_class_children(self, node: str, names: bool = True) -> list[str]:
+        """Returns a list of children to the node class.
+        
+        Parameters
+        ----------
+        node : str
+            The name of the class of interest. Can be the class id,
+            or class name -- class_id always starts with func_, so 
+            it is easy to parse.
+        names : bool, default True
+            If True, outputs the names of classes. Else outputs the func_ids.
+            
+        Returns
+        -------
+        list[str]
+            The list of children of the functional use class node.
+        """
+        if node.startswith("func_"):
+            self.cursor.execute("""
+                SELECT child_id FROM ClassificationHierarchy
+                WHERE ((parent_id is ?) AND (child_id IS NOT NULL));               
+                """, (node,))    
+        else:
+            self.cursor.execute("""
+                SELECT child_id FROM ClassificationHierarchy
+                WHERE parent_id = (
+                    SELECT id FROM Classifications
+                    WHERE classification = ?
+                );
+                """, (node,))
+        
+        parents = set()
+        for row in self.cursor.fetchall():
+            parents.add(row[0])
+            
+        parents = list(parents)
+        
+        if names:
+            new_parents = set()
+            for parent_id in parents:
+                self.cursor.execute("""
+                    SELECT classification FROM Classifications
+                    WHERE id = ?;                
+                    """, (parent_id,))
+                new_parents.add(self.cursor.fetchone()[0])
+            
+            parents = list(new_parents)
+            
+        parents.sort()
+        
+        return parents
+            
+if __name__ == "__main__":
+    pass
+    
